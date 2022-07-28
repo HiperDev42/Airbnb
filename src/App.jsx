dfd8c9ccbb1d02a272811b01baec27b5776f411b
HiperDev42 <fekelemen@gmail.com>
2022-07-28 20:07:02 -0300

    Add filter modal

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './NavBar'
 import Card from './Card'
 import Map from './Map'
 import { cards } from './Database'
+import FilterModal from './components/FilterModal'
 console.log(cards)
 
 function App() {
@@ -14,13 +15,14 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <FilterModal />
       <main className='container-fluid p-0'>
         <div className="row g-0 m-0">
           <div className="col p-0">
           <div className='container-fluid p-0'>
               <div className="d-flex p-2 sticky-top justify-content-between filter-bar bg-white">
                 <b>Mais de mil acomodações</b>
-                <button type='button' className='btn btn-outline-dark'><i class="fa-solid fa-filter"></i>Filtros</button>
+                <button type='button' className='btn btn-outline-dark' data-toggle='modal' data-target='#filter-modal'><i class="fa-solid fa-filter"></i>Filtros</button>
               </div>
               
             <div className="row">
