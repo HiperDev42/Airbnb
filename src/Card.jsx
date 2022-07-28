a4269fcd575ab453b29edeb8a952915aebb80a11
HiperDev42 <fekelemen@gmail.com>
2022-07-28 03:00:05 -0300

    Add some styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,9 +6,9 @@ function Card(props) {
         <div className="card">
             <img src={props.loc.image} />
             <div className='grid'>
-                <div className="title">{props.loc.title}</div>
+                <div className="title one-line">{props.loc.title}</div>
                 <div className='rating'><span className='fa fa-star'>5,0 (5)</span></div>
-                <div className='description'>{props.loc.description}</div>
+                <div className='description one-line'>{props.loc.description}</div>
                 <div className='pricing'>R$ {props.loc.price}</div>
             </div>
         </div>
